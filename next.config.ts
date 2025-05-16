410763eb37053c13b35a0964cf3f32c7e72867c2
Ian M. Nava G <72428415+ianmnv@users.noreply.github.com>
2025-05-16 20:20:40 +0200

    Configure next.config correctly to manage Images properly

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   images: {
-    domains: ["cdn.dummyjson.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+        port: "",
+        pathname: "/product-images/**",
+        search: "",
+      },
+    ],
   },
 };
 
